eab16875009c92f30456faf2262a5b955a107a71
Harsha Goli <harshagoli@gmail.com>
2017-12-19 19:07:52 -0500

    Created WalletElement class

---
+++ app/ui/screens/wallets/WalletElement.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Button, Platform, NativeModules, StatusBar, Keyboard} from 'react-native';
+
+
+export default class WalletElement extends React.Component {
+
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#212d40'
+    },
+});
