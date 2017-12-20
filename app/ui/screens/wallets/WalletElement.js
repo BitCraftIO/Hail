54922a8b84783b3c99c12d2796bc69126692a825
Harsha Goli <harshagoli@gmail.com>
2017-12-19 20:49:07 -0500

    Added static wallets view

--- app/ui/screens/wallets/WalletElement.js
+++ app/ui/screens/wallets/WalletElement.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import {StyleSheet, Text, View, TextInput, Button, Platform, NativeModules, StatusBar, Keyboard} from 'react-native';
+import PropTypes from 'prop-types';
 
+export default class WalletElement extends React.PureComponent {
 
-export default class WalletElement extends React.Component {
+    constructor() {
+    	super()
+    }
 
+	static propTypes = {
+		symbol: PropTypes.string.isRequired,
+        aggregateCoins: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        aggregateValue: PropTypes.number.isRequired,
+        percentageGrowth: PropTypes.number.isRequired
+    }
+
+	render() {
+		console.log("Rendering WalletElement")
+		return (
+			<View style={styles.background}>
+				<Text>{this.props.symbol}</Text>
+				<Text>{this.props.aggregateCoins}</Text>
+				<Text>{this.props.name}</Text>
+				<Text>{this.props.aggregateValue}</Text>
+				<Text>{this.props.percentageGrowth}</Text>
+			</View>
+		);
+	};
 }
 
 const styles = StyleSheet.create({
--- app/ui/screens/wallets/WalletsList.js
+++ app/ui/screens/wallets/WalletsList.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput, Button, Platform, NativeModules, StatusBar, Keyboard} from 'react-native';
-
+import {FlatList, StyleSheet, Text, View, TextInput, Button, Platform, NativeModules, StatusBar, Keyboard} from 'react-native';
+import WalletElement from "./WalletElement.js"
 
 export default class WalletsList extends React.Component {
+
+    walletDATA = [
+        {
+            symbol: "BTC",
+            aggregateCoins: 1.34,
+            name: "Personal Wallet",
+            aggregateValue: 15.123,
+            percentageGrowth: 53,
+            key: 1
+        },
+        {
+            symbol: "ETH",
+            aggregateCoins: 0.56,
+            name: "Public Wallet",
+            aggregateValue: 141.23,
+            percentageGrowth: -53,
+            key: 2
+        },
+        {
+            symbol: "XMR",
+            aggregateCoins: 300.34,
+            name: "Savings",
+            aggregateValue: 150.000,
+            percentageGrowth: 400,
+            key: 3
+        },
+    ]
+
 	static navigationOptions = {
         header: () => {
         }
@@ -11,8 +39,8 @@ export default class WalletsList extends React.Component {
     constructor() {
         super();
         this.state = {
-            "localWallets": false,
-            "walletData": [],
+            "localWallets": true,
+            "walletData": this.walletDATA,
             "exchangeWallets": false,
             "exchangeData":[]
         };
@@ -20,15 +48,29 @@ export default class WalletsList extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-               
+            <View style={styles.background}>
+                <FlatList
+                    data={this.state.walletData}
+                    renderItem={(wallet) => 
+                        
+                        <WalletElement 
+                            symbol={wallet.item.symbol} 
+                            aggregateCoins={wallet.item.aggregateCoins}
+                            name={wallet.item.name}
+                            aggregateValue={wallet.item.aggregateValue}
+                            percentageGrowth={wallet.item.percentageGrowth}
+                        />
+                        
+                    
+                    }
+                />
             </View>
         );
-    }
+    };
 }
 
 const styles = StyleSheet.create({
-    container: {
+    background: {
         flex: 1,
         backgroundColor: '#11151c'
     },
