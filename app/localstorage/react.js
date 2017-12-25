dc8f4560fbe79563e07071fa11502df68cbf63f5
Harsha Goli <harshagoli@gmail.com>
2017-12-24 19:54:01 -0500

    Implemented creatWalletLocally

--- app/localstorage/react.js
+++ app/localstorage/react.js
@@ -1,3 +1,5 @@
+// @flow
+
 const Realm = require('realm');
 
 const walletSchema = {
@@ -6,7 +8,7 @@ const walletSchema = {
 		network: 'string',
 		name: 'string',
 		masterKey: 'string',
-		id: {type: 'int', default: },
+		id: {type: 'int', default: 0},
 		receiveAddresses: 'string[]',
 		changeAddress: 'string',
 		transactions: 'Transaction[]'
@@ -18,7 +20,45 @@ const transactionSchema = {
 	properties: {
 		hash: 'string',
 		fee: 'int',
-		input: 'string[]'
+		input: 'string[]',
 		output: 'string[]'
 	}
+};
+
+export function createWalletLocally(options) : Promise<boolean> {
+	Realm.open({schema: [walletSchema, transactionSchema]})
+	.then(realm => {
+		realm.write(() => {
+			realm.create('Wallet', {
+				network: options.network,
+				name: options.name,
+				masterKey: options.masterKey,
+				receiveAddresses: options.receiveAddresses,
+				changeAddress: options.changeAddress
+			})
+		})
+	})
+	.then(() => {
+		return true
+	})
+	.catch((err) => {
+		return false
+	})
+
+}
+
+export function deleteWalletLocallyById(id) {
+
+}
+
+export function addAddressToWallet(wallet, address) {
+
+}
+
+export function addTxToWalletById(tx, id) {
+
+}
+
+export function addTxToWalletByAddress(tx, id) {
+
 }
