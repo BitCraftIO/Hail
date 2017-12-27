42fd21481585f64eca3b6b96f3346df451679d2b
Harsha Goli <harshagoli@gmail.com>
2017-12-27 13:19:53 -0500

    Finished simple MakeOrReceiveTransactionPage

--- app/ui/screens/wallets/MakeOrReceiveTransactionFlow/MakeOrReceiveTransactionPage.js
+++ app/ui/screens/wallets/MakeOrReceiveTransactionFlow/MakeOrReceiveTransactionPage.js
@@ -5,14 +5,59 @@ import PropTypes from 'prop-types';
 export default class MakeOrReceiveTransactionPage extends React.Component {
 	constructor(props){
 		super(props)
+		this.state = {
+			"text": "Type your Address here"
+		}
+	}
+
+	goToNextPage(address) {
+		const {navigate} = this.props.navigation;
+        navigate("MakeOrReceiveTransactionInputAmountPage", {"address": this.state.text});
 	}
 
 	render() {
 		return (
 			<View>
-				<Text>wallet</Text>
+				<View
+					style={styles.inputBox}
+				>
+					<TextInput
+						multiline = {false}
+						onChangeText={(text) => this.setState({text})}
+						value={this.state.text}
+					/>
+				</View>
+				<View>
+					<Button
+						title = {"Next"}
+						onPress = {() => this.goToNextPage()} 
+					/>
+				</View>
 			</View>
 		)
 	}
 
 }
+
+styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        //backgroundColor: '#11151c',
+        alignItems: 'center',
+        backgroundColor: '#11151c',
+    },
+    inputBox: {
+    	borderColor: '#000000',
+
+    },
+    // list: {
+    //     paddingTop: 70,
+    // },
+    // sectionHeader: {
+    //     fontFamily: "Avenir",
+    //     fontSize: 9,
+    //     color: "#9b9b9b",
+    //     paddingTop: 30,
+    //     paddingBottom: 15
+    // },
+});
