e487804d9bd124ddc56b3a09debab26efa9e87e9
Harsha Goli <harshagoli@gmail.com>
2017-12-28 01:03:07 -0500

    Fixed navigator probnlem

--- app/Navigator.js
+++ app/Navigator.js
@@ -4,8 +4,8 @@ import Testbed from "./ui/screens/Testbed";
 import WalletsList from "./ui/screens/wallets/WalletsList";
 import Watchlist from "./ui/screens/watchlist/state/WatchlistContainer";
 import WalletDetailsPage from "./ui/screens/wallets/WalletDetailsPage";
-import MakeOrReceiveTransactionPage from "./ui/screens/wallets/MakeOrReceiveTransactionPage";
-
+import MakeOrReceiveTransactionPage from "./ui/screens/wallets/MakeOrReceiveTransactionFlow/MakeOrReceiveTransactionPage";
+import MakeOrReceiveTransactionInputAmountPage from "./ui/screens/wallets/MakeOrReceiveTransactionFlow/MakeOrReceiveTransactionInputAmountPage"
 
 // https://reactnavigation.org/docs/intro/basic-app
 const Navigator = StackNavigator({
@@ -30,6 +30,9 @@ const Navigator = StackNavigator({
     },
     MakeOrReceiveTransactionPage: {
     	screen: MakeOrReceiveTransactionPage
+    },
+    MakeOrReceiveTransactionInputAmountPage: {
+        screen: MakeOrReceiveTransactionInputAmountPage
     }
 });
 
