06ff3c0a23945556a97c61b5c660c97f427ea77b
Harsha Goli <harshagoli@gmail.com>
2018-01-09 16:50:56 -0500

    Added network codes

---
+++ app/localstorage/utils/networkcodes.js
@@ -0,0 +1,13 @@
+export const localCodes = {
+    "BTC":"0001",
+    "LTC":"0002",
+
+    "0001":"BTC",
+    "0002":"LTC",
+}
+
+export const exchangeCodes = {
+    "Bitfinex":"0001",
+    
+    "0001":"Bitfinex"
+}
