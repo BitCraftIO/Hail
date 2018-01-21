3ee97962b88d253151dd58a154e89a18820caefc
Harsha Goli <harshagoli@gmail.com>
2018-01-20 21:11:07 -0500

    Added Linking

--- app/App.js
+++ app/App.js
@@ -2,12 +2,39 @@ import React from 'react';
 import Navigator from "./Navigator"
 import { Provider } from 'react-redux'
 import createStore from "./reduxhelpers/CreateStore"
+import DeepLinking from 'react-native-deep-linking';
 
 let store = createStore();
 
 export default class App extends React.Component {
 
- 	
+ 	componentDidMount() {
+		DeepLinking.addScheme('hail://');
+		
+		DeepLinking.addRoute('/wallet/:id', (response) => {
+			this.setState(response);
+		});
+
+		DeepLinking.addRoute('/wallet/setup/coinbase/continue', (state, code) => {
+			//TODO: Fix this? Yea
+			console.log(state, code, "this is hitting");
+			// const {navigate} = this.props.navigation;
+        	// navigate("WalletDetailsPage", {"wallet": wallet, refresh: this.refresh});
+		});
+	}
+
+	componentWillUnmount() {
+		Linking.removeEventListener('url', this.handleUrl);
+	}
+
+	handleUrl = ({ url }) => {
+		Linking.canOpenURL(url).then((supported) => {
+			if (supported) {
+				DeepLinking.evaluateUrl(url);
+			}
+		});
+	  }
+
 	render() {
 		return (
 			<Provider store={store}>
