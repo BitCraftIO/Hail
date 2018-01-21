4ffc3b22ef09892eb96a6ed27ff9f28b21de55a5
Harsha Goli <harshagoli@gmail.com>
2018-01-20 22:27:08 -0500

    commiting for checkout

--- app/App.js
+++ app/App.js
@@ -3,14 +3,17 @@ import Navigator from "./Navigator"
 import { Provider } from 'react-redux'
 import createStore from "./reduxhelpers/CreateStore"
 import DeepLinking from 'react-native-deep-linking';
+import { Linking } from 'react-native';
 
 let store = createStore();
 
 export default class App extends React.Component {
 
  	componentDidMount() {
+		alert("ComponentDidMount");
 		DeepLinking.addScheme('hail://');
-		
+		Linking.addEventListener('url', this.handleUrl);
+
 		DeepLinking.addRoute('/wallet/:id', (response) => {
 			this.setState(response);
 		});
@@ -18,21 +21,32 @@ export default class App extends React.Component {
 		DeepLinking.addRoute('/wallet/setup/coinbase/continue', (state, code) => {
 			//TODO: Fix this? Yea
 			console.log(state, code, "this is hitting");
-			// const {navigate} = this.props.navigation;
-        	// navigate("WalletDetailsPage", {"wallet": wallet, refresh: this.refresh});
+			// alert("this is hitting");
+			const {navigate} = this.props.navigation;
+        	navigate("WalletsList");
 		});
+
+		Linking.getInitialURL().then((url) => {
+			if (url) {
+			  Linking.openURL(url);
+			}
+		  }).catch(err => console.error('An error occurred', err));
 	}
 
 	componentWillUnmount() {
+		//TODO: Find out why this isn't solving the memory leak
 		Linking.removeEventListener('url', this.handleUrl);
 	}
 
 	handleUrl = ({ url }) => {
-		Linking.canOpenURL(url).then((supported) => {
-			if (supported) {
-				DeepLinking.evaluateUrl(url);
-			}
-		});
+
+		alert("handleURL");
+
+		// Linking.canOpenURL(url).then((supported) => {
+		// 	if (supported) {
+		// 		DeepLinking.evaluateUrl(url);
+		// 	}
+		// });
 	  }
 
 	render() {
