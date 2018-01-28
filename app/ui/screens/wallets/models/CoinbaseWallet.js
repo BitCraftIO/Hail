fab2a94faea43e0bbe5c0bc62b999d4912e99b8c
Harsha Goli <harshagoli@gmail.com>
2018-01-28 15:36:53 -0500

    schema changes

--- app/ui/screens/wallets/models/CoinbaseWallet.js
+++ app/ui/screens/wallets/models/CoinbaseWallet.js
@@ -4,11 +4,13 @@ import CoinbaseAPI from "../network/exchanges/CoinbaseAPI"
 
 export default class CoinbaseWallet extends Realm.Object{}
 CoinbaseWallet.schema = {
-	name: "CoinbaseWallet",
-	primaryKey: "id",
-	properties: {
+    name: "CoinbaseWallet",
+    primaryKey: "id",
+    properties: {
         id: "int",
         apiKey: "string?",
-        oauth2Key: "string?",
-	}
+        //Sometimes called bearer token
+        accessToken: "string?",
+        refreshToken: "string?",
+    }
 };
