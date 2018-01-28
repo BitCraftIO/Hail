ed6111b079292b1875e40ca7f338acc37426886f
Zach Bloomquist <zbloomq@live.com>
2018-01-28 14:28:35 -0500

    Add bitcoinjs-lib, change globals to get bitcoinjs-lib to work

--- global.js
+++ global.js
@@ -3,7 +3,21 @@
 
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+
+// TODO: use secure random number generator for this
+var getRandomValues = function (byteArray) {
+    for (let i = 0; i < byteArray.length; i++) {
+        byteArray[i] = Math.floor(256 * Math.random());
+    }
+}
+// "But Zach, aren't you just doing the same thing twice?"
+// Yes. Initializing the crypto-browserify module eventually requires
+// crypto.getRandomValues to exist, so we must add it here once.
+// However, crypto-browserify does not support getRandomValues, so we
+// must re-add it after loading the module.
+global.crypto = { getRandomValues }
 global.crypto = require('crypto');
+global.crypto.getRandomValues = getRandomValues;
 
 global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
 
