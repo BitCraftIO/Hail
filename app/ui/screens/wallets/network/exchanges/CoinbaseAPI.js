2d2f38a9b81b3f398bc16b525aa3ebd17dadcf89
Harsha Goli <harshagoli@gmail.com>
2018-01-28 15:36:37 -0500

    added some api functions

--- app/ui/screens/wallets/network/exchanges/CoinbaseAPI.js
+++ app/ui/screens/wallets/network/exchanges/CoinbaseAPI.js
@@ -1,5 +1,6 @@
 import {Linking} from 'react-native';
 
+//TODO: save these creds in a config file
 const clientId = "e6f835288c8bcecd7dfafb7e9a073b2b83d95b6a1f4f5f6fefeca0f345a93326";
 const clientSecret = "2a9617bfc208fa9b7c796916c6ed8f2bd51a4b61add007e4d342d7f6ace2fe2f";
 const appUri = "";
@@ -18,4 +19,37 @@ export function redirectToOAuth() {
         console.log("Shit ain't supported LOL ")
     });
     return success;
+}
+
+export function listAccounts(accessToken) {
+    fetch('https://api.coinbase.com/v2/accounts', {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        }
+    })
+}
+
+export function getAccessToken(auth_code){
+    fetch('POST https://api.coinbase.com/oauth/token', {
+        method: 'POST',
+        headers: {},
+        body: JSON.stringify({
+            grant_type: 'authorization_code',
+            code: auth_code,
+            client_id: clientId,
+            client_secret: clientSecret,
+            redirect_uri: 'hail://wallet/oauth/coinbase/redirect'
+        })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+        return {
+            accessToken: responseJson.access_token,
+            refreshToken: responseJson.refresh_token,
+        }
+    })
+    .catch(e => {
+        console.log(e);
+    })
 }
