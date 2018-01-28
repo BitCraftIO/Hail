f832a454732ec02197cdf19714f9a7a873cbb24d
Harsha Goli <harshagoli@gmail.com>
2018-01-27 21:20:36 -0500

    Added new values to schema

--- app/ui/screens/wallets/models/CoinbaseWallet.js
+++ app/ui/screens/wallets/models/CoinbaseWallet.js
@@ -1,14 +1,21 @@
 import Db from "hail/app/localstorage/db/Db";
 import Realm from "realm";
+import CoinbaseAPI from "../network/exchanges/CoinbaseAPI"
 
-export default class CoinbaseWallet extends Realm.Object{}
+export default class CoinbaseWallet extends Realm.Object{
+
+    authorize() {
+        if (!coinbaseAPI.redirectToOAuth()) {
+            alert("auth failed!");
+        }
+    }
+}
 CoinbaseWallet.schema = {
 	name: "CoinbaseWallet",
 	primaryKey: "id",
 	properties: {
         id: "int",
-        apiKey: "string",
-        oauth2Key: "string",
-        
+        apiKey: "string?",
+        oauth2Key: "string?",
 	}
 };
