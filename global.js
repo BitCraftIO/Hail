987316132ed89730fff26c4ddc7b934fc07ec4e7
Zach Bloomquist <zbloomq@live.com>
2018-01-28 13:08:53 -0500

    Add eth-lightwallet & fixes for injecting crypto into global scope

--- global.js
+++ global.js
@@ -1,19 +1,13 @@
 // Inject node globals into React Native global scope.
+// Required for crypto functionality for bitcoinjs-lib, web3, etc.
+
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+global.crypto = require('crypto');
+
 global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
 
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
     protocol: 'file:',
-};
-
-// Don't do this in production. You're going to want to patch in
-// https://github.com/mvayngrib/react-native-randombytes or similar.
-global.crypto = {
-    getRandomValues(byteArray) {
-        for (let i = 0; i < byteArray.length; i++) {
-            byteArray[i] = Math.floor(256 * Math.random());
-        }
-    },
 };
