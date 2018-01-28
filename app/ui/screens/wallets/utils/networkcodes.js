a4ca07376bae62b4cf7fef1e5bc94991227d0035
Harsha Goli <harshagoli@gmail.com>
2018-01-27 21:20:57 -0500

    Added Coinbase codes

--- app/ui/screens/wallets/utils/networkcodes.js
+++ app/ui/screens/wallets/utils/networkcodes.js
@@ -1,3 +1,4 @@
+//implementationCode is always the key
 export const coinToCode = {
     "BTC":"0001",
     "LTC":"0002",
@@ -10,8 +11,10 @@ export const codeToCoin = {
 
 export const exchangeToCode = {
     "Bitstamp":"0001",
+    "Coinbase":"0002",
 }
 
 export const codeToExchange = {
     "0001":"Bitstamp",
+    "0002":"Coinbase",
 }
