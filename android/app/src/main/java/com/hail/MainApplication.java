0fae727227e21e82b9b8f6df82d8600b16566def
Zach Bloomquist <zbloomq@live.com>
2018-01-31 16:42:58 -0500

    Installing library for native RNG

--- android/app/src/main/java/com/hail/MainApplication.java
+++ android/app/src/main/java/com/hail/MainApplication.java
@@ -3,6 +3,7 @@ package com.hail;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.peel.react.TcpSocketsModule;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new TcpSocketsModule(),
             new RealmReactPackage(),
             new VectorIconsPackage(),
