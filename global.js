cea847763a0a5bfba3a70e7dc846c4bc22779415
Zach Bloomquist <zbloomq@live.com>
2018-01-31 22:17:51 -0500

    Secure-er randomBytes implementation
    Fixes #3

--- global.js
+++ global.js
@@ -2,12 +2,14 @@
 // Required for crypto functionality for bitcoinjs-lib, web3, etc.
 
 global.Buffer = require('buffer').Buffer;
-global.process = require('process');
 
-// TODO: use secure random number generator for this
+global.process = require('process');
+global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
+
 var getRandomValues = function (byteArray) {
+    var bytes = crypto.rng.randomBytes(byteArray.length);
     for (let i = 0; i < byteArray.length; i++) {
-        byteArray[i] = Math.floor(256 * Math.random());
+        byteArray[i] = bytes[i];
     }
 }
 // "But Zach, aren't you just doing the same thing twice?"
@@ -15,11 +17,11 @@ var getRandomValues = function (byteArray) {
 // crypto.getRandomValues to exist, so we must add it here once.
 // However, crypto-browserify does not support getRandomValues, so we
 // must re-add it after loading the module.
-global.crypto = { getRandomValues }
+global.crypto = { getRandomValues };
 global.crypto = require('crypto');
 global.crypto.getRandomValues = getRandomValues;
-
-global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
+global.crypto.rng = require('react-native-randombytes');
+crypto.rng.seedSJCL();
 
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
