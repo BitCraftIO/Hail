0bb91d4730125e17d02273057d6cf43a488b7188
Harsha Goli <harshagoli@gmail.com>
2018-02-06 02:01:42 -0500

    Removed unneeded statements

--- app/ui/screens/wallets/network/exchanges/coinbase/CoinbaseAPI.js
+++ app/ui/screens/wallets/network/exchanges/coinbase/CoinbaseAPI.js
@@ -1,23 +1,5 @@
 import {Linking} from 'react-native';
-
-//TODO: save these creds in a config file
-const clientId = "e6f835288c8bcecd7dfafb7e9a073b2b83d95b6a1f4f5f6fefeca0f345a93326";
-const clientSecret = "2a9617bfc208fa9b7c796916c6ed8f2bd51a4b61add007e4d342d7f6ace2fe2f";
-const appUri = "";
-const webUri = `https://www.coinbase.com/oauth/authorize?client_id=${clientId}&response_type=code&scope=`
-                //scopes
-                +`wallet:accounts:read,`
-                +`wallet:accounts:create,`
-                +`wallet:accounts:update,`
-                +`wallet:addresses:read,`
-                +`wallet:addresses:create,`
-                +`wallet:buys:read,`
-                +`wallet:buys:create,`
-                +`wallet:deposits:read,`
-                +`wallet:user:email,`
-                +`wallet:withdrawals:read,`
-                +`wallet:transactions:read,`
-                +`wallet:transactions:send,`
+import {clientId, clientSecret, appUri, webUri} from './config';
 
 export function redirectToOAuth() {
     var success = false;
