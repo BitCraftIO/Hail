04d12f1cf05e1cb553f304e010d560c775d51cae
Harsha Goli <harshagoli@gmail.com>
2018-02-06 00:51:31 -0500

    TO ALL WHO DOUBTED HARSHA. HE HAS CONQUERED ASYNC. HE HAS CONQUERED SHIT ERRORHANDLING. HERE YE HERE YE \༼ຈلຈ༽ﾉ \༼ຈلຈ༽ﾉ \༼ຈلຈ༽ﾉ

--- app/ui/screens/wallets/newwalletflow/coinbasesuccesspage/CoinbaseSuccessPageState.js
+++ app/ui/screens/wallets/newwalletflow/coinbasesuccesspage/CoinbaseSuccessPageState.js
@@ -14,119 +14,58 @@ export const PAGE = "CoinbaseSuccessPage";
 export function SuccessCoinbasePageActions(dispatch) {
     return {
         createCoinbaseWallet: createResourceAction(dispatch, CREATE_REALM_COINBASE_WALLET, CREATE_REALM_COINBASE_WALLET_TAG, async (code) => {
-            const {accessToken, refreshToken} = await getAccessToken(code);
-            // console.log(accessToken);
-            // const w = listAccounts(accessToken);
-            // w.then(accounts => {
-            //     //const transactions = listTransactions(accounts[0].id, accessToken)
-            //     const transactions = fetch(`https://api.coinbase.com/v2/accounts/${accounts[0].id}/transactions`, {
-            //         method: 'GET',
-            //         headers: {
-            //             Authorization: `Bearer ${accessToken}`,
-            //         }
-            //     })
-            //     .then((response) => response.json())
-            //     .then((response) => {
-            //         console.log(response)
-            //         return response;
-            //     })
-            //     .catch(e => {
-            //         throw new Error("listTransaction FAILED!")
-            //     })
-            //     console.log(transactions);
-                // Promise.all(accounts.map(({id, name, currency, balance}) => {
-                //     const transactions = listTransactions(id, accessToken)
-                //     // transactions.then((txs) => {
-                //     //     txs.map(({id, type, status, amount, description, to}) => {
-                //     //         console.log(id, type, status, amount, description, to)
-                //     //     })
-                //     // })
-                //     //const cb = new CoinbaseAccount(id, name, currency, balance);
-                //     //console.log(cb);
-                //     transactions.then((txs) => console.log(txs));
-                //     return {id, name, currency, balance, transactions}
-                // })).then((accounts) => console.log(accounts));
-            //})
-            listAccounts(accessToken)
-            .then(accounts => {
-                return Promise.all(accounts.map(({id, name, currency, balance}) => {
-                    return new CoinbaseAccount({
-                        id, 
-                        name, 
-                        currency, 
-                        balance, 
-                        transactions: listTransactions(id, accessToken)
+            // A small moment of silence to all those who perished in the battles taken place below. 
+            const {accessToken, refreshToken} = await getAccessToken(code);   
+            return { 
+                id: listAccounts(accessToken).then(accounts => {
+                    return Promise.all(accounts.map((account) => {
+                        return listTransactions(account.id, accessToken)
                         .then(txs => {
                             return txs.map(({id, type, status, amount, description, to}) => {
-                                return new CoinbaseTransaction({
+
+                                //'to' doesn't appear on buys
+                                if (type == 'buy') {
+                                    to = {
+                                        id: 'Account',
+                                        resource: type,
+                                    }
+                                }
+                                if (to.address) {
+                                    to.id = to.address;
+                                }
+                                //if more version of 'to' appear, add elseifs here to handle
+                                return {
                                     id,
                                     type,
-                                    amount: amount.amount,
+                                    amount: Number(amount.amount),
                                     currency: amount.currency,
-                                    description: description.title,
+                                    //description: description? description.title : "Unavailable",
                                     to: to.id,
                                     toType: to.resource,
-                                })
+                                };
                             })
                         })
+                        .then(transactions => {
+                            return {
+                                id: account.id,
+                                name: account.name, 
+                                currency: account.currency,
+                                balance: Number(account.balance.amount),
+                                transactions,
+                            };
+                        })
+                    }))
+                })
+                .then(accounts => {   
+                    return actions.createWallet({
+                        network: "Coinbase",
+                        name: "Coinbase",
+                        accessToken,
+                        refreshToken,
+                        accounts,
                     })
-                }))
-            })
-            .then(accounts => {
-                console.log(accounts)
-            })
-            // const cbwallet = actions.createWallet({
-            //     network: "Coinbase",
-            //     name: "Coinbase",
-            //     accessToken,
-            //     refreshToken,
-            //     accounts,
-            // })  
-            // console.log(cbwallet)
-
-            //v1
-            // const accounts = await listAccounts(accessToken)
-            
-
-            // const id = actions.createWallet({
-            //     network: "Coinbase",
-            //     name: "Coinbase",
-            //     accessToken,
-            //     refreshToken,
-            // });
-
-            // const cbwallet = queries.getWalletbyId(id)[0];
-            // const accounts = await listAccounts(cbwallet.accessToken)
-            // Promise.all(accounts.map(async ({id, name, currency, balance}) => {
-            // // const accounts = (await listAccounts(cbwallet.accessToken)).map( async ({id, name, currency, balance}) => {
-            //     const transactions = await listTransactions(id, cbwallet.accessToken)
-            //     Promise.all(transactions.map(({id, type, status, amount, description, to}) => {
-            //         return new CoinbaseTransaction({
-            //             id,
-            //             type,
-            //             amount: amount.amount,
-            //             currency: amount.currency,
-            //             description: description.title,
-            //             to: to.id,
-            //             toType: to.resource,
-            //         });
-            //     }))
-            //     .then( cbtx => {
-            //         return new CoinbaseAccount({
-            //             id, 
-            //             name,
-            //             currency,
-            //             transactions: cbtx,
-            //         });
-            //     })
-            // }))
-            // .then(accounts => actions.updateWalletById(id, { accounts }))
-            // .catch(e => console.log(e));
-
-            // return {
-            //     success: true,
-            //     cbwallet,
-            // }
+                })
+            }
         })
     }
 }
