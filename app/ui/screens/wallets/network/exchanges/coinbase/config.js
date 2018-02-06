2bc8364a327d99d610c0dd2ba36a407b4a1b79d8
Harsha Goli <harshagoli@gmail.com>
2018-02-06 01:56:38 -0500

    minor fix

--- app/ui/screens/wallets/network/exchanges/coinbase/config.js
+++ app/ui/screens/wallets/network/exchanges/coinbase/config.js
@@ -1,7 +1,7 @@
 export const clientId = "e6f835288c8bcecd7dfafb7e9a073b2b83d95b6a1f4f5f6fefeca0f345a93326";
 export const clientSecret = "2a9617bfc208fa9b7c796916c6ed8f2bd51a4b61add007e4d342d7f6ace2fe2f";
 export const appUri = "";
-export const webUri = `https://www.coinbase.com/oauth/authorize?client_id=${clientId}&response_type=code&scope=${scopes.join(',')}&${sendLimit.join('&')}`;
+export const webUri = `https://www.coinbase.com/oauth/authorize?client_id=${clientId}&response_type=code&scope=${scopes.join(',')}` //&${sendLimit.join('&')}`;
 
 const scopes = [
     `wallet:accounts:read`,
@@ -18,7 +18,7 @@ const scopes = [
     //`wallet:transactions:send`,
 ]             
 
-//TODO: For right now sending tx's don't work since coinbase's systems won't let us do that without more auth
+//TODO: For right now sending tx's don't work since coinbase's systems won't let us do that without more auth.
 const sendLimits = [
     //For new applications, meta[send_limit_amount] is limited to $1 per user.
     //TODO: Ask for more permissions in account settings 
