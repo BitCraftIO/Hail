13b4fc3af57fe2770a91afd0e9a57101ad0354b7
Harsha Goli <harshagoli@gmail.com>
2018-02-16 01:03:24 -0500

    Added documentation and some functions

--- app/ui/screens/wallets/utils/Ethereum.js
+++ app/ui/screens/wallets/utils/Ethereum.js
@@ -4,7 +4,10 @@ import ethWallet, {
 import {
     pubToAddress,
 } from 'ethereumjs-util';
+import web3 from '../network/coins/Ethereum/Web3';
 
+
+// TODO: move this to /app/screen/wallets/crypto
 export default class Ethereum {
 
     /**
@@ -25,4 +28,26 @@ export default class Ethereum {
     generateAddress(privateKey) {
         return pubToAddress(privateKey, true).toString();
     }
+    
+    /**
+     * 
+     * @param {*} privateKey 
+     * @param {*} fromAddress 
+     * @param {*} toAddress 
+     * @param {*} gasLimit 
+     * @param {*} gasPrice 
+     */
+    createRawTransaction(options) {
+
+    }
+
+    _buildTransaction() {
+
+    }
+
+    _signTransaction() {
+
+    }
+
+
 }
