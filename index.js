791af1c8ec87dc4454d39977e3959b25b0a9d1f8
Harsha Goli <harshagoli@gmail.com>
2018-02-17 23:16:37 -0500

    Fixed buffer issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require('./global');
 import { AppRegistry } from 'react-native';
 import App from './App';
-require('./global')
-
+import ethwallet from 'ethereumjs-wallet'
 AppRegistry.registerComponent('Hail', () => App);
