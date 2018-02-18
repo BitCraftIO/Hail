a273e06c8ec548621f61c1b0c7712186b7a292ee
Harsha Goli <harshagoli@gmail.com>
2018-02-17 23:33:50 -0500

    creates ethereum private key pair

---
+++ app/ui/screens/wallets/utils/WalletActions.js
@@ -0,0 +1,40 @@
+import bip44hdkey from 'ethereumjs-wallet/hdkey';
+import ethwallet from 'ethereumjs-wallet';
+import ethtx from 'ethereumjs-tx';
+import bip39 from 'bip39';
+
+
+export function send() {
+
+}
+
+export function createPrivateKeyPair(network) {
+    switch(network) {
+        case "BTC":
+            break;
+        case "LTC":
+            break;
+        case "ETH":
+            const mnemonic = bip39.generateMnemonic();
+            const newAccount = bip44hdkey.fromMasterSeed(mnemonic);
+            const keynode = newAccount.derivePath("m/44'/60'/0'/0");
+            const address = keynode.deriveChild(0);
+            console.log(address);
+            return {
+                masterKey: bip39.mnemonicToSeedHex(mnemonic),
+                mnemonic: mnemonic,
+                address: "m/44'/60'/0'/0 "+address,
+            }
+            break;
+        default: 
+            break;
+    }
+}
+
+export function createPrivateKey() {
+
+}
+
+export function createAddress() {
+
+}
