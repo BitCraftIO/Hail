d2e29b430b9030b4f284e96bf0693285eb72929b
Harsha Goli <harshagoli@gmail.com>
2018-02-24 14:18:52 -0500

    Added prettify

--- app/ui/screens/wallets/utils/WalletActions.js
+++ app/ui/screens/wallets/utils/WalletActions.js
@@ -6,29 +6,32 @@ import bip39 from 'bip39';
 export function send() {}
 
 export function createPrivateKeyPair(network) {
-  switch (network) {
-    case 'BTC':
-      break;
-    case 'LTC':
-      break;
-    case 'ETH':
-      const mnemonic = bip39.generateMnemonic();
-      console.log(bip39.mnemonicToSeed(mnemonic));
-      const newAccount = bip44hdkey.fromMasterSeed(
-        bip39.mnemonicToSeed(mnemonic)
-      );
-      const keynode = newAccount.derivePath("m/44'/60'/0'/0");
-      const address = keynode.deriveChild(0);
-      console.log(address);
-      return {
-        masterKey: bip39.mnemonicToSeedHex(mnemonic),
-        mnemonic: mnemonic,
-        address: "m/44'/60'/0'/0 " + address
-      };
-      break;
-    default:
-      break;
-  }
+    switch (network) {
+        case 'BTC':
+            break;
+        case 'LTC':
+            break;
+        case 'ETH':
+            //TODO: Move this into Ethereum.js
+            const mnemonic = bip39.generateMnemonic();
+            console.log(bip39.mnemonicToSeed(mnemonic));
+            const newAccount = bip44hdkey.fromMasterSeed(
+                bip39.mnemonicToSeed(mnemonic)
+            );
+            const keynode = newAccount.derivePath("m/44'/60'/0'/0");
+            const address = keynode.deriveChild(0);
+            console.log(address);
+
+            //Note: this is all incorrect if you see this comment
+            return {
+                masterKey: bip39.mnemonicToSeedHex(mnemonic),
+                mnemonic: mnemonic,
+                address: "m/44'/60'/0'/0 " + address.publicExtendedKey()
+            };
+            break;
+        default:
+            break;
+    }
 }
 
 export function createPrivateKey() {}
