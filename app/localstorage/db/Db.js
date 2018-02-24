296f5fa8e651f9d4713b6f36d6baed6bfa082049
Harsha Goli <harshagoli@gmail.com>
2018-02-24 14:16:58 -0500

    Better error message on fail

--- app/localstorage/db/Db.js
+++ app/localstorage/db/Db.js
@@ -1,66 +1,64 @@
-import Realm from "realm";
-import config from "./config";
-import BTCTransaction from "hail/app/ui/screens/wallets/models/BTCTransaction";
-import LTCTransaction from "hail/app/ui/screens/wallets/models/LTCTransaction";
-import BTCWallet from "hail/app/ui/screens/wallets/models/BTCWallet";
-import LTCWallet from "hail/app/ui/screens/wallets/models/LTCWallet";
-import ETHWallet from "../../ui/screens/wallets/models/ETHWallet";
-import ETHTransaction from "../../ui/screens/wallets/models/ETHTransaction";
+import Realm from 'realm';
+import config from './config';
+import BTCTransaction from 'hail/app/ui/screens/wallets/models/BTCTransaction';
+import LTCTransaction from 'hail/app/ui/screens/wallets/models/LTCTransaction';
+import BTCWallet from 'hail/app/ui/screens/wallets/models/BTCWallet';
+import LTCWallet from 'hail/app/ui/screens/wallets/models/LTCWallet';
+import ETHWallet from '../../ui/screens/wallets/models/ETHWallet';
+import ETHTransaction from '../../ui/screens/wallets/models/ETHTransaction';
 
 export let realm = new Realm({
-	schema: [
-		BTCWallet, 
-		BTCTransaction, 
-		LTCWallet, 
-		LTCTransaction, 
-		ETHWallet,
-		ETHTransaction,
-	],
-	path: config.db_path,
+    schema: [
+        BTCWallet,
+        BTCTransaction,
+        LTCWallet,
+        LTCTransaction,
+        ETHWallet,
+        ETHTransaction
+    ],
+    path: config.db_path
 });
 
 export function query(model, filter) {
-	let results = this.realm.objects(model);
-	if(filter) {
-		return results.filtered(filter);
-	}
-	return results;
-};
+    let results = this.realm.objects(model);
+    if (filter) {
+        return results.filtered(filter);
+    }
+    return results;
+}
 
 export function insert(model, options) {
-	this.write(() => {
-		realm.create(model, options);
-	});
-};
+    this.write(() => {
+        realm.create(model, options);
+    });
+}
 
 export function del(model, obj) {
-	this.write(() => {
-		realm.delete(obj);
-	});
-};
+    this.write(() => {
+        realm.delete(obj);
+    });
+}
 
 export function update(obj, options) {
-	this.write(() => {
-		Object.keys(options).map((key, attribute) => {
-			obj[key] = attribute;
-		});
-	});
-};
+    this.write(() => {
+        Object.keys(options).map((key, attribute) => {
+            obj[key] = attribute;
+        });
+    });
+}
 
-export function write(func){
-	try {
-		realm.write(func);
-	} catch(e) {
-		throw new Error('Db.js :: Write operation failed');
-	}
-};
+export function write(func) {
+    try {
+        realm.write(func);
+    } catch (e) {
+        throw new Error('Db.js :: Write operation failed ' + e);
+    }
+}
 
 export function close() {
-	Realm.close()
-};
+    Realm.close();
+}
 
-export function createRealm() {
-
-};
+export function createRealm() {}
 
 console.log(realm.path);
