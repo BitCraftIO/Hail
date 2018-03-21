d3605f2a9b12f3de32cd32bac66546786bae4a56
Harsha Goli <harshagoli@gmail.com>
2018-03-21 00:19:54 -0400

    Created file to house all walletactions and seperate functionality based on coin

--- app/ui/screens/wallets/utils/WalletActions.js
+++ app/ui/screens/wallets/utils/WalletActions.js
@@ -1,9 +1,65 @@
-import Ethereum from '../crypto/Ethereum.js';
+import * as Ethereum from '../crypto/Ethereum.js';
+import web3 from '../network/coins/Ethereum/Web3';
+import { Alert } from 'react-native';
 
-export function send() {}
+/**
+ *
+ * @param {*} coin
+ * @param {*} network
+ * //Ethereum
+ * @param {string} params.nonce must have 0x preceeding
+ * @param {string} params.privateKey
+ * @param {string} params.to destination address
+ * @param {string} params.gasLimit must have 0x preceeding
+ * @param {string} params.gasPrice must have 0x preceeding
+ * @param {string} params.value must have 0x preceeding
+ * @param {int} params.chainId EIP 155 chainId - mainnet: 1, ropsten: 3
+ */
+export function send(coin, params, network = 'main') {
+    switch (coin) {
+        case 'BTC':
+            break;
+        case 'ETH':
+            web3.eth.getBalance(params.from).then((error, result) => console.log(error, result));
+            const rawTx = Ethereum.createRawTransaction(params);
+            Alert.alert('Ready to Send?', 'This will send a transaction across testnet. Are you ready?', [
+                {
+                    //TODO: Replace console.log with proper logger
+                    text: 'Send',
+                    onPress: () => {
+                        web3.eth.sendSignedTransaction(`0x${rawTx.toString('hex')}`, (error, result) => {
+                            if (error) {
+                                console.log(`Error: ${error}`);
+                            } else {
+                                console.log(`Result: ${result}`);
+                                return result;
+                            }
+                        });
+                    }
+                }
+            ]);
+            break;
+        default:
+            break;
+    }
+}
 
-export function createPrivateKeyPair(network) {
-    switch (network) {
+export function estimateFee(coin, from, to, value) {
+    switch (coin) {
+        case 'ETH':
+            return web3.eth.estimateGas({ from, to });
+
+            break;
+        default:
+    }
+}
+
+export function getGasPrice() {
+    return web3.eth.getGasPrice();
+}
+
+export function createPrivateKeyPair(coin, network = 'main') {
+    switch (coin) {
         case 'BTC':
             break;
         case 'LTC':
