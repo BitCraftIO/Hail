946cf4d4d622402deae9e701384ee672c7b9f1f9
Harsha Goli <harshagoli@gmail.com>
2018-03-22 16:28:32 -0400

    Added Realm Log

---
+++ app/localstorage/db/models/Log.js
@@ -0,0 +1,23 @@
+import Realm from 'realm';
+
+export default class Log extends Realm.Object {}
+Log.schema = {
+    name: 'Log',
+    primaryKey: 'date',
+    properties: {
+        date: 'date',
+        logLevel: 'int',
+        component: 'string',
+        message: 'string'
+    }
+};
+
+/*
+    Log Levels | default setting
+
+    0 - error    log
+    1 - warn     log
+    2 - info     log
+    3 - debug    off
+    4 - trace    off
+*/
