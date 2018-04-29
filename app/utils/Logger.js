a496aa5731f89716e63419fb9e67134f68bb5b13
PhlipPhlops <phillkin@gmail.com>
2018-04-28 21:33:57 -0400

    Classified Logger.js and added some documentation on the different log levels :unicorn:

--- app/utils/Logger.js
+++ app/utils/Logger.js
@@ -1,19 +1,60 @@
 import * as Db from '../localstorage/db/Db';
+import Log from '../localstorage/db/models/Log';
 import * as settings from './Settings';
+import moment from 'moment';
 
-export function logger(logLevel, message) {
-    const date = new Date(Date.now());
-    if (logLevel <= settings.configFile.logLevel) {
-        const id = highestId + 1;
-        Db.insert('Log', { id, date, logLevel, message });
-        console.log(`${date} : ${logLevel} :: ${message}`);
-    } else if (logLevel == 4) {
-        console.log(`${date} : ${logLevel} :: ${message}`);
+export class Logger {
+    levelMap = {
+        0: 'Error',
+        1: 'Notify',
+        2: 'Info'
+    }
+
+    /**
+     * Log for errors that the user cares about
+     * Eg. "Transaction Failed"
+     * @param {String} message 
+     */
+    error(message) {
+        this._storeLog(message, 0);
+    }
+
+    /**
+     * Log for updates the user would want to know about
+     * Eg. "Transaction Confirmed" or "Transaction Received"
+     * @param {String} message 
+     */
+    notify(message) {
+        this._storeLog(message, 1);
+    }
+    
+    /**
+     * Log for important status updates that the user might not care about
+     * Eg. "Sync Manager Started" or "BTC Sync Complete"
+     * @param {String} message 
+     */
+    info(message) {
+        this._storeLog(message, 2);
+    }
+
+    /**
+     * Stores log messages to be read chronologically later
+     * @param {String} message 
+     * @param {number} logLevel 
+     */
+    _storeLog(message, logLevel) {
+        let timestamp = moment().format(),
+            date = timestamp.toDate();
+
+        if (logLevel <= settings.configFile.logLevel) {
+            let logObject = {
+                date,
+                logLevel,
+                message
+            }
+            
+            Db.insert(Log, logObject);
+        }
+        console.log(`${timestamp} : ${this.levelMap[logLevel]} :: ${message}`)
     }
 }
-
-export function readFromLog() {}
-
-var highestId = () => {
-    return Db.query('Log').sorted('id', true)[0].id;
-};
