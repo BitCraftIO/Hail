e2395cd62e62fca595514696ddda5bc795f3073c
Caleb Lewis <caleb_dre@yahoo.com>
2018-05-26 15:19:20 -0400

    removed old wallet stuff

--- app/Navigator.js
+++ app/Navigator.js
@@ -5,12 +5,10 @@ import WalletDashboard from "./ui/screens/wallets/dashboard/WalletDashboard";
 import WalletDetailsPage from './ui/screens/wallets/WalletDetailsPage';
 import WalletPager from './ui/screens/wallets/dashboard/WalletPager';
 import TransactionPage from './ui/screens/wallets/transactionflow/TransactionPage';
-import NewWalletPage from './ui/screens/wallets/newwalletflow/NewWalletPage';
 import CoinbaseSuccessPage from './ui/screens/wallets/newwalletflow/coinbasesuccesspage/CoinbaseSuccessPageContainer';
 import LogPage from './ui/screens/logger/LogPage';
 import SettingsPage from './ui/screens/settings/SettingsPage';
 import ModifySettingsScreen from './ui/screens/settings/ModifySettingsScreen';
-
 // https://reactnavigation.org/docs/intro/basic-app
 const Navigator = StackNavigator({
     Home: {
