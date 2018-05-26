55627654d613f8c2069dd0c71c0fa2e344c18e0f
Harsha Goli <harshagoli@gmail.com>
2018-05-26 15:26:36 -0400

    rewrote using native hashing and no bitcoinjs-lib for wallet or address generation

--- app/crypto/coins/Bitcoin.js
+++ app/crypto/coins/Bitcoin.js
@@ -1,56 +1,92 @@
-import { ECPair, HDNode, TransactionBuilder } from 'bitcoinjs-lib';
+import TransactionBuilder from 'bitcoinjs-lib/transaction_builder';
 const bip39 = require('bip39');
+import createHash from 'create-hash';
+import bs58check from 'bs58check';
+import * as hdutil from './util/hd';
 
-// TODO: move this to /app/screen/wallets/crypto
+export function send(params, network) {}
 
 /**
- * Util class for dealing with calculations for any Bitcoin-based coin.
- * Does not keep track of wallets.
+ * @param masterKey
+ * @param address
  */
-export default class Bitcoin {
-    /**
-     * Create a new instance of the Bitcoin util class.
-     * @param {*} network Network used for operations in this instance.
-     */
-    constructor(network) {
-        this.network = Bitcoin._getNetworkFromKey(network);
-    }
+export function generateHDWallet() {
+    const wallet = hdutil.generateHDWallet(0);
+    const address = this.generateAddressFromNode(wallet.addressNode);
 
-    /**
-     * Generates a BIP39 mnemonic and a corresponding HD node.
-     */
-    getRandomMnemonic() {
-        return bip39.generateMnemonic();
-    }
-
-    /**
-     * Returns true if a valid mnemonic is supplied.
-     */
-    validateMnemonic(mnemonic) {
-        properLength = mnemonic.trim().split(/\s+/g).length >= 12;
-        return bip39.validateMnemonic(mnemonic) && properLength;
-    }
-
-    /**
-     * Returns the HD node from a supplied BIP39 mnemonic.
-     */
-    getHDNodeFromMnemonic(mnemonic) {
-        if (!this.validateMnemonic(mnemonic)) throw new Error('Invalid mnemonic passed.');
-        seed = bip39.mnemonicToSeed(mnemonic);
-        return HDNode.fromSeedBuffer(seed, this.network);
-    }
-
-    deriveHDNodeFromParent(parentHDNode, index) {
-        return parentHDNode.derive(index);
-    }
-
-    /**
-     * Resolve the `network` parameter into a member of `bitcoin.networks`.
-     *
-     * @param key Whatever key Harsha creates
-     */
-    static _getNetworkFromKey(key) {
-        // TODO: actually implement this based off whatever harsha does
-        return bitcoinjs.networks.bitcoin;
-    }
+    /*
+        for now we must save the extended private key and generate the private key from that
+        as hdkey has no good way of beginning from a private key. 
+    */
+    return {
+        privateKey: wallet.root._hdkey.privateKey.toString('hex'),
+        extendedPrivateKey: wallet.root.privateExtendedKey(),
+        addresses: [{ string: address, derivationPath: "m/44'/0'/0'/0/0 " }]
+    };
 }
+
+/**
+ *
+ * @param {string} from hex
+ * @param {string} to hex
+ * @param {string} data hex
+ */
+export function estimateFee(from, to = '4584158529818ef77D1142bEeb0b6648BD8eDb2f', data) {}
+
+/**
+ *
+ * @param {string} privateKey
+ */
+export function generateAddressForIndex(wallet, index) {
+    const node = hdutil.privateKeyToAddrNode(wallet.privateKey);
+    return this.generateAddressFromNode(node, index, wallet);
+}
+
+/**
+ * Ensure that the node passed through has a path similar to m/purpose/cointype/0'/0
+ * Make sure user knows that address = account so this should only be used to create a new wallet
+ * with the same priv key as another
+ * @param {hdkey} node
+ * @param {int} index
+ */
+export function generateAddressFromNode(node, index = 0, wallet) {
+    const addrNode = node.derive(index);
+    return createBitcoinAddressI(addrNode._publicKey);
+}
+
+function createBitcoinAddress(privateKey) {
+    const step1 = privateKey;
+    const step2 = createHash('sha256')
+        .update(step0)
+        .digest();
+    const step3 = createHash('rmd160')
+        .update(step1)
+        .digest();
+    var step4 = Buffer.allocUnsafe(21);
+    step4.writeUInt8(wallet.network == 'MAIN' ? 0x00 : 0x6f, 0);
+    step3.copy(step4, 1); //step3 now holds the extended RIPMD-160 result
+    var step9 = bs58check.encode(step4);
+}
+
+/**
+ *
+ * @param {string} privateKey
+ * @param {string} password the password to encrypt the keystore to
+ */
+export function generateKeyStore(privateKey, password) {}
+
+export function generateWalletFromKey() {
+    //TODO:
+}
+
+/**
+ * https://github.com/ethereumjs/ethereumjs-tx#usage
+ * @param {string} nonce must have 0x preceeding
+ * @param {string} privateKey
+ * @param {string} to destination address
+ * @param {string} gasLimit must have 0x preceeding
+ * @param {string} gasPrice must have 0x preceeding
+ * @param {string} value must have 0x preceeding
+ * @param {int} chainId EIP 155 chainId - mainnet: 1, ropsten: 3
+ */
+export function createRawTransaction(options) {}
