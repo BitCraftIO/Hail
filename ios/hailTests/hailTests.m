c2f72482fb6e82f3daca49dfe2a53b960fce38cd
Harsha Goli <harshagoli@gmail.com>
2018-07-23 23:16:58 -0400

    Fixed Phillips silly issue

--- ios/hailTests/hailTests.m
+++ ios/hailTests/hailTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface hailTests : XCTestCase
+@interface HailTests : XCTestCase
 
 @end
 
-@implementation hailTests
+@implementation HailTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
